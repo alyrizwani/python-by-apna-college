a8a027a91603cca7e4b34562bd0c57d3fd3faf91
Ali Raza <rixwani39g@gmail.com>
2026-01-29 02:33:07 +0500

    q2.py

--- Lecture 3/q2.py
+++ Lecture 3/q2.py
@@ -1,4 +1,14 @@
+#Program to check if a list is PALINDROME or NOT
 list = [6,7,0,1,2,3,2,1,0,7,6]
+
+list1 = list.copy()
+list1.reverse()
+if list == list1:
+    print("The list is a palindrome")
+else:
+
+    print("The list is not a palindrome")
+
 # length = len(list) - 1
 # # print(length)
 # mid = int(length / 2)
@@ -10,9 +20,3 @@ list = [6,7,0,1,2,3,2,1,0,7,6]
 #     print("The list is a palindrome")
 # else:
 #     print("The list is not a palindrome")
-list1 = list.copy()
-list1.reverse()
-if list == list1:
-    print("The list is a palindrome")
-else:
-    print("The list is not a palindrome")
